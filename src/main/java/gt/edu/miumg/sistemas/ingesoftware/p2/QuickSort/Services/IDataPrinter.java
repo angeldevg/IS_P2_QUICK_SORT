23e80494ab329e9c28d0f61483a78bca1f597125
angeldevg <angel.proyectosumg@gmail.com>
2021-04-25 23:39:39 -0600

    Se agrego las clases perteneciente al algoritmo de ordenamiento rapido

---
+++ src/main/java/gt/edu/miumg/sistemas/ingesoftware/p2/QuickSort/Services/IDataPrinter.java
@@ -0,0 +1,8 @@
+package gt.edu.miumg.sistemas.ingesoftware.p2.QuickSort.Services;
+
+
+public interface IDataPrinter {
+    
+     public void PrintWords(String words[]);
+    
+}
---
+++ src/main/java/gt/edu/miumg/sistemas/ingesoftware/p2/QuickSort/Services/IQuickSort.java
@@ -0,0 +1,9 @@
+
+package gt.edu.miumg.sistemas.ingesoftware.p2.QuickSort.Services;
+
+
+public interface IQuickSort {
+    
+    public  void quicksort(String words[], int left, int right);
+    
+}
